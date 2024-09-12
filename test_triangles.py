20bd8698d563aca38b3193d7a6f65c222225f02e
Tom Fyfe <tomfyfe85@gmail.com>
2024-09-12 15:20:15 +0100

    #triangle_type_machine passes #test_error_with_less_than_three_coords

--- test_triangles.py
+++ test_triangles.py
@@ -1,2 +1,10 @@
-def test_triangles():
-    pass
+from triangles import triangle_type_machine
+import pytest
+
+def test_error_with_less_than_three_coords():
+    """Exception raised if there are less than 3 coordinates"""
+    with pytest.raises(Exception) as e:
+        triangle_type_machine([(0, 0), (0, 1)])
+    error_msg = str(e.value)
+    assert error_msg == "A triangle has must only have 3 coordinates"
+
--- triangles.py
+++ triangles.py
@@ -1,3 +1,19 @@
+"""triangle_type_machine confirms type of triangle from coordinates passed as an argument"""
+
 def triangle_type_machine(coordinates):
-    # confirms type of triangle from coordinates
-    pass
+    if  len(coordinates) < 3:
+        raise Exception("A triangle has must only have 3 coordinates")
+
+
+
+
+# find length of sides from coordinates
+# use to determine isosceles, equilateral
+
+# 1. Equilateral Triangle
+# Definition: A triangle in which all three sides are equal in length,
+# and all three internal angles are equal, each measuring 60°.
+
+# 2. Isosceles Triangle
+# Definition: A triangle in which at least two sides are of equal length,
+# and the angles opposite those sides are equal.
